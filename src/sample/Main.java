9612b0f72aa1011c70657129c04c0bc05f10d83f
fabian bienz <fabian-bienz@users.noreply.github.com>
2018-10-24 13:02:35 +0200

    changes to layout

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,13 +4,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.MenuBar;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+	    Parent root = loader.load();
+	    MenuBar menuBar = (MenuBar)loader.getNamespace().get("menuBar");
+	    menuBar.prefWidthProperty().bind(primaryStage.widthProperty());
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 650, 500));
         primaryStage.show();
