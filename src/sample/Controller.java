d62963042dad49a2d1210506a60334f2800138c9
fabian bienz <fabian-bienz@users.noreply.github.com>
2018-10-31 10:08:09 +0100

    tasks can be moved

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 
 public class Controller {
 	
@@ -10,8 +11,38 @@ public class Controller {
 	private TextField newTask;
 	
 	@FXML
+	private VBox tbd;
+	
+	@FXML
+	private VBox inProgress;
+	
+	@FXML
+	private VBox done;
+	
+	@FXML
 	private void addTask(ActionEvent e) {
 		System.out.print(newTask.getText());
+		TextField task = new TextField(newTask.getText());
+		task.setEditable(false);
+		task.setOnMouseClicked(event -> {
+			tbd.getChildren().remove(task);
+			inProgress.getChildren().add(task);
+			newEvent1(task);
+		});
+		tbd.getChildren().addAll(task);
 	}
 	
+	private void newEvent1(TextField task) {
+		task.setOnMouseClicked(event -> {
+			inProgress.getChildren().remove(task);
+			done.getChildren().add(task);
+			newEvent2(task);
+		});
+	}
+	
+	private void newEvent2(TextField task) {
+		task.setOnMouseClicked(event -> {
+			done.getChildren().remove(task);
+		});
+	}
 }
