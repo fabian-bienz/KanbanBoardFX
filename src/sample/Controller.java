c5a8e52d14d56a50a5c3364a97aa37577f278536
fabian bienz <fabian-bienz@users.noreply.github.com>
2018-10-31 09:32:56 +0100

    added controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,17 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class Controller {
+	
+	@FXML
+	private TextField newTask;
+	
+	@FXML
+	private void addTask(ActionEvent e) {
+		System.out.print(newTask.getText());
+	}
+	
 }
